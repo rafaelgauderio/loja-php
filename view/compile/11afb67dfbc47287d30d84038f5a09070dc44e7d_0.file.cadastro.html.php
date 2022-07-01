56ae41653e02f31eefaf4dadec2f30d973287bae
Rafael de Luca <deluca1712@gmail.com>
2022-07-01 16:24:17 -0300

    layout pagina cadastro

--- view/compile/11afb67dfbc47287d30d84038f5a09070dc44e7d_0.file.cadastro.html.php
+++ view/compile/11afb67dfbc47287d30d84038f5a09070dc44e7d_0.file.cadastro.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.45, created on 2022-06-30 18:49:59
+/* Smarty version 3.1.45, created on 2022-07-01 21:23:07
   from 'C:\xampp\htdocs\loja\view\cadastro.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.45',
-  'unifunc' => 'content_62bdd4375aab13_60514609',
+  'unifunc' => 'content_62bf499b36d841_07006581',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '11afb67dfbc47287d30d84038f5a09070dc44e7d' => 
     array (
       0 => 'C:\\xampp\\htdocs\\loja\\view\\cadastro.html',
-      1 => 1656607793,
+      1 => 1656618455,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_62bdd4375aab13_60514609 (Smarty_Internal_Template $_smarty_tpl) {
+function content_62bf499b36d841_07006581 (Smarty_Internal_Template $_smarty_tpl) {
 ?><h4 class="alert alert-success">Favor preencher os campos abaixos.</h4>
 <h4 class="alert alert-danger">Apenas email e senha são obrigatórios. Mínimo 6 dígitos na senha.</h4>
 
@@ -40,17 +40,22 @@ function content_62bdd4375aab13_60514609 (Smarty_Internal_Template $_smarty_tpl)
         <input type="text" name="client_nome" class="form-control">
     </div>
 
-    <div class="col-md-6">
+    <div class="col-md-5">
         <label>Endereço: </label>
         <input type="text" name="client_endereco" class="form-control">
     </div>
-    <div class="col-md-6">
+    <div class="col-md-1">
         <label>Número: </label>
         <input type="number" name="client_numero" class="form-control">
     </div>
     <div class="col-md-6">
         <label>Cep: </label>
         <input type="number" name="client_cep" class="form-control">
+    </div>
+
+    <div class="col-md-6">
+        <label>Bairro: </label>
+        <input type="text" name="client_bairro" class="form-control">
     </div>
 
     <div class="col-md-6">
@@ -72,7 +77,7 @@ function content_62bdd4375aab13_60514609 (Smarty_Internal_Template $_smarty_tpl)
 
 <section class="row" id="salvar_cliente">
     <div class="col-md-6">
-        <button type="submit" class="btn btn-success btn-block"> Salvar Cadastro </button>
+        <button type="submit" class="btn btn-success btn-lg btn-block"> Salvar Cadastro </button>
     </div>
     <br>
     <br>
